bed39ff3f15cf33f654ff3e9e2c2eee4af43ad32
subbum <subbu@gmail.com>
2023-06-16 18:32:20 +0100

    vars changed to var reference

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 resource "aws_docdb_cluster" "main" {
-  cluster_identifier      = "${vars.env}-docdb"
+  cluster_identifier      = "${var.env}-docdb"
   engine                  = var.engine
-  engine_version            = var.engine_version
+  engine_version          = var.engine_version
   master_username         = data.aws_ssm_parameter.user.value
   master_password         = data.aws_ssm_parameter.user.value
   backup_retention_period = var.backup_retention_period
   preferred_backup_window = var.preferred_backup_window
   skip_final_snapshot     = var.skip_final_snapshot
   db_subnet_group_name    = aws_docdb_subnet_group.main.name
-  kms_key_id = data.aws_kms_key.key.arn
-  storage_encrypted =   var.aws_docdb_subnet_group
+  kms_key_id              = data.aws_kms_key.key.arn
+  storage_encrypted       = var.aws_docdb_subnet_group
 }
 
 /* #When we need to create Cluster we need minimum two available zones, 
